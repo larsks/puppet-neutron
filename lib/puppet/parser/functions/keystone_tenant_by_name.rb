bf02a4c5a1faee978e5a0afb5912ee9b49b29c3d
Lars Kellogg-Stedman <lars@redhat.com>
2014-04-17 13:27:53 -0400

    rewrote without fog
    Change-Id: I7ced41cfdd4a2fc758b80712410bb5acf3daabc0

--- lib/puppet/parser/functions/keystone_tenant_by_name.rb
+++ lib/puppet/parser/functions/keystone_tenant_by_name.rb
@@ -1,4 +1,60 @@
-require 'fog'
+require 'rubygems'
+require 'net/http'
+require 'json'
+
+def keystone_v2_authenticate(auth_url,
+                             username,
+                             password,
+                             tenantId=nil,
+                             tenantName=nil)
+
+    post_args = {
+        'auth' => {
+            'passwordCredentials' => {
+                'username' => username,
+                'password' => password
+            },
+        }}
+
+    if tenantId
+        post_args['auth']['tenantId'] = tenantId
+    end
+    
+    if tenantName
+        post_args['auth']['tenantName'] = tenantName
+    end
+
+    puts post_args
+    
+    url = URI.parse("#{auth_url}/tokens")
+    req = Net::HTTP::Post.new url
+    req['content-type'] = 'application/json'
+    req.body = post_args.to_json
+
+    res = Net::HTTP.start(url.hostname, url.port) {|http|
+        http.request(req)
+    }
+
+    data = JSON.parse res.body
+    return data['access']['token']['id'], data
+end
+
+def keystone_v2_tenants(auth_url,
+                        token)
+
+    url = URI.parse("#{auth_url}/tenants")
+    req = Net::HTTP::Get.new url
+    req['content-type'] = 'application/json'
+    req['x-auth-token'] = token
+
+    res = Net::HTTP.start(url.hostname, url.port) {|http|
+        http.request(req)
+    }
+
+    data = JSON.parse res.body
+    puts data
+    data['tenants']
+end
 
 module Puppet::Parser::Functions
 
@@ -8,14 +64,14 @@ module Puppet::Parser::Functions
         password = args[2]
         tenant_name = args[3]
 
-        keystone = Fog::Identity.new :provider => 'OpenStack',
-                                     :openstack_auth_url => auth_url,
-                                     :openstack_username => username,
-                                     :openstack_api_key => password
+        token, authinfo = keystone_v2_authenticate(auth_url,
+                                                   username,
+                                                   password)
 
+        tenants = keystone_v2_tenants(auth_url, token)
+        selected = tenants.select{|tenant| tenant['name'] == tenant_name}
 
-        tenant = keystone.tenants.find{ |t| t.name == tenant_name }
-        tenant.id
+        return selected.length == 1 ? selected[0]['id'] : nil
     end
 
 end
